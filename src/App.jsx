f10f12a4271eeabee8e444d34f1eb6b72aa89d9c
Pushpendra Singh <contactpushpendra19@gmail.com>
2026-03-02 14:35:37 +0530

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Checkout from './pages/checkout'
-import Auth from './pages/auth'
+import Auth from './pages/Auth'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import AuthProvider from './context/AuthContext'
 
 function App() {
 //  const [count, setCount] = useState(0)
 
   return (
     <div className='app'>
+      <AuthProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
+      </AuthProvider>
     </div>
   )
 }
